a3907413193de23ffcf8446fed5fcf158148ca9b
i has candys <ichen10@stuy.edu>
2020-12-20 15:25:22 -0500

    implemented selectionSort and tested many random cases

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Sorts {
 	/**Bubble sort of an int array. 
 	*@postcondition The array will be modified such that the elements will be in increasing order.
@@ -17,6 +18,24 @@ public class Sorts {
 				}
 			}
 			j--;
+		}
+	}
+	public static void selectionSort(int[] data) {
+		for (int i = 0; i < data.length; i++) {
+			int smallestElement = data[i];
+			int indexOfSwap = i;
+			for (int j = i+1; j < data.length; j++) {
+				if (data[j] < smallestElement) {
+					smallestElement = data[j];
+					indexOfSwap = j;
+				}
+			}
+			if (smallestElement != data[i]) {
+				int temp = data[i];
+				data[i] = smallestElement;
+				data[indexOfSwap] = temp;
+				// System.out.println(Arrays.toString(data));
+			}
 		}
 	}
 }
